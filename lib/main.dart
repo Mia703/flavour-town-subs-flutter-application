9cc30529329d8c9a394693c55ebbf2bbde5b34f9
Amya Moore <bookworm7572@gmail.com>
2024-07-05 14:45:24 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World! This is a test'),
+          child: Text('Hello there, is this working?'),
         ),
       ),
     );
